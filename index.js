9eaf1473c07492c7b2e4464de124c928e61afbc6
khan faysal <mdfaysalkhancse@gmail.com>
2023-07-28 18:36:35 +0600

    backend server create for tech build pc project

---
+++ index.js
@@ -0,0 +1,102 @@
+require('dotenv').config();
+const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.brk1j.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect(); // Connect to MongoDB
+
+    const db = client.db('build-tech-pc');
+    const productCollection = db.collection('build-pc');
+
+    app.get('/products', async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+
+      res.send({ status: true, data: products });
+    });
+
+    app.post('/product', async (req, res) => {
+      const product = req.body;
+
+      const result = await productCollection.insertOne(product);
+
+      res.send(result);
+    });
+
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const result = await productCollection.findOne({ _id: ObjectId(id) });
+   
+      res.send(result);
+    });
+
+    app.delete('/product/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const result = await productCollection.deleteOne({ _id: ObjectId(id) });
+   
+      res.send(result);
+    });
+
+    app.post('/comment/:id', async (req, res) => {  
+      const productId = req.params.id;
+      const comment = req.body.comment;
+
+      const result = await productCollection.updateOne(
+        { _id: ObjectId(productId) },
+        { $push: { comments: comment } }
+      );
+
+      if (result.modifiedCount !== 1) {
+        console.error('Product not found or comment not added');
+        res.json({ error: 'Product not found or comment not added' });
+        return;
+      }
+
+      console.log('Comment added successfully');
+      res.json({ message: 'Comment added successfully' });
+    });
+
+    app.get('/comment/:id', async (req, res) => {
+      const productId = req.params.id;
+
+      const result = await productCollection.findOne(
+        { _id: ObjectId(productId) },
+        { projection: { _id: 0, comments: 1 } }
+      );
+
+      if (result) {
+        res.json(result);
+      } else {
+        res.status(404).json({ error: 'Product not found' });
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+
+    console.log('Connected to MongoDB successfully');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+};
+
+run().catch((err) => console.error('Error in run:', err));
